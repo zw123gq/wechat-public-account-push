d6aec934c4d2f5ef31fc883aec6a9599a1d16c53
zw123gq <113829890+zw123gq@users.noreply.github.com>
2022-09-18 18:25:52 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "wx2c1cff65156058ec",
+  APP_SECRET: "0bcbc1ded0fff80fe8e9e29e06b0300c",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
